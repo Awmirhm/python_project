43001ebb9222514d8d63ac83e8bcf208fefb30d6
Amir Hossien <96288225+Awmirhm@users.noreply.github.com>
2024-04-27 13:16:11 +0330

    Delete Projects/UserManagement directory

--- Projects/UserManagement/CommonLayer/logs_class.py
+++
@@ -1,6 +0,0 @@
-class Logs:
-    def __init__(self, id, function_name, call_data_time, execution_time):
-        self.id = id
-        self.function_name = function_name
-        self.call_data_time = call_data_time
-        self.execution_time = execution_time
--- Projects/UserManagement/CommonLayer/manger_decorator.py
+++
@@ -1,7 +0,0 @@
-def manager_decorator(function):
-    def wrapper(*args, **kwargs):
-        result = function(*args, **kwargs)
-        if result == "amir.hm":
-            print("Invalid")
-
-    return wrapper
